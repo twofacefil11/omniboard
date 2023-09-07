5f9db07c7fba0a7cb81bf7df74e189e7fbeb1e6a
twofacefil11 <twofacefil11@gmail.com>
2023-09-07 22:11:51 +0000

    Invalid DateTime

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ createBoard();
 const pawnSpawners = [{spawner : pawnSpawn[0], color : blacks},
                       {spawner : pawnSpawn[1], color : whites}];
 
-activateSpawner(pawnSpawners[0], r);
-activateSpawner(pawnSpawners[1], r);
+activateSpawner(pawnSpawners[0], er);
+activateSpawner(pawnSpawners[1], er);
 
 //spawn Blacks and drag the copies
 // pawnSpawn[0].addEventListener('mousedown', function(event) {
