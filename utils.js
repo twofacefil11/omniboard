dca5aa27187ea037a4150282b44953e899c2a6ed
twofacefil11 <twofacefil11@gmail.com>
2023-09-07 22:12:14 +0000

    Invalid DateTime

--- utils.js
+++ utils.js
@@ -40,7 +40,7 @@ function activateSpawner(spawner, r) {
       const spawnPosition = getAbsolutePosition(spawner.spawner.children[0]);
       mousePressed = true;
 
-      pawnsArr.push(new Pawn(spawner.color, "pawn", e, spawnPosition.left, spawnPosition.top, idIt()));
+      pawnsArr.push(new Pawn(spawner.color, "pawn", r, spawnPosition.left, spawnPosition.top, idIt()));
       dragged = pawnsArr[pawnsArr.length - 1];
       document.body.appendChild(dragged.actualThing);
 
