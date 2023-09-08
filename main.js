fdfd5ef253aec9e9d833c7af1a09a339ea0629a3
twofacefil11 <twofacefil11@gmail.com>
2023-09-08 11:45:42 +0000

    Invalid DateTime

--- main.js
+++ main.js
@@ -185,7 +185,7 @@ function createBoard() {
 }
 
 function makeDropZone(dropzone) {
-    dropzone.style.width = (window.innerWidth * 0.9).toString() + 'px';
+    dropzone.style.width = (window.innerWidth * 0.94).toString() + 'px';
     dropzone.style.height = (window.innerHeight * 0.8).toString() + 'px';
     dropzone.style.left = (window.innerWidth * 0.05).toString() + "px";
     dropzone.style.top = ((window.innerHeight * 0.15)).toString() + "px";
