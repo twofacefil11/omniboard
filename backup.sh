4964ab148333af8723405b02d4841e93064ca2c3
twofacefil11 <twofacefil11@gmail.com>
2023-09-08 13:43:36 +0000

    Invalid DateTime

--- backup.sh
+++ backup.sh
@@ -3,9 +3,9 @@
 backup_branch="backup"
 
 git checkout main
-git branch "$backup_branch"
-git checkout "$backup_branch"
-git push origin "$backup_branch"
+git branch "$backup"
+git checkout "$backup"
+git push origin "$backup"
 git checkout main
 
 echo "Backup done, back to main"
