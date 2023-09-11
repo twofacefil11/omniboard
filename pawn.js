97ced0d20884bddb14df18a4e542d5153dd73c41
twofacefil11 <twofacefil11@gmail.com>
2023-09-11 21:00:42 +0000

    Invalid DateTime

--- pawn.js
+++ pawn.js
@@ -226,13 +226,13 @@ class Pawn {
         // this.actualThing.style.top = '0px';
     }
 
-    // mouseMove() {
-    //     console.log('mousemove')
-    //     if (mousePressed && dragged) {
-    //       dragged.style.left = `${event.clientX - er}px`;
-    //       dragged.style.top = `${event.clientY - er}px`;
-    //     }
-    //   }
+    mouseMove() {
+        console.log('mousemove')
+        if (mousePressed && dragged) {
+          this.actualThing.style.left = `${event.clientX - er}px`;
+          this.actualThing.style.top = `${event.clientY - er}px`;
+        }
+      }
     // letGoListener() {
     //     document.addEventListener('mouseup', function() {
     //         if (dragged) {
