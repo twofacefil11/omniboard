ce168f8b954e5e275a2ed208e52b1cae84aaca44
twofacefil11 <twofacefil11@gmail.com>
2023-09-11 20:56:38 +0000

    Invalid DateTime

--- utils.js
+++ utils.js
@@ -16,7 +16,7 @@ function deHex(hex) {
   function onMouseMove() {
     console.log('mousemove')
     if (mousePressed && dragged) {
-      dragged.actualThing.style.left = `${event.clientX - er}px`;
+      dragged.style.left = `${event.clientX - er}px`;
       dragged.actualThing.style.top = `${event.clientY - er}px`;
     }
   }
