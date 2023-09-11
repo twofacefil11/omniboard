511948b3aa6c89cb8438f9075c6e3489f2cc4179
twofacefil11 <twofacefil11@gmail.com>
2023-09-11 21:00:11 +0000

    Invalid DateTime

--- pawn.js
+++ pawn.js
@@ -117,7 +117,7 @@ class Pawn {
             mousePressed = true;
             dragged = this.actualThing;
             event.preventDefault;
-            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mousemove', mouseMove);
         })
     }
 
