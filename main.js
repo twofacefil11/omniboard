d705938a75b6ff19210ac6dbde64a9929689d80d
twofacefil11 <twofacefil11@gmail.com>
2023-09-11 20:40:06 +0000

    Invalid DateTime

--- main.js
+++ main.js
@@ -54,8 +54,7 @@ document.addEventListener('mouseup', function() {
         
         document.removeEventListener('mousemove', onMouseMove);
         
-        dragged.draggable = true;
-        console.log(dragged.draggable);
+        dragged.setAttribute('draggable', 'true')
         if (!(dragged.isOnBoard())) 
               dragged.makeItFall();
 
