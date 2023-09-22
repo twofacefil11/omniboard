3917ffcd8782efad30c37b150633862c1f88c95e
twofacefil11 <twofacefil11@gmail.com>
2023-09-22 21:10:08 +0000

    Invalid DateTime

--- listeners.js
+++ listeners.js
@@ -20,7 +20,7 @@ function activateSpawner(spawner, color) {
 }
 
 //I want this to take care of freashly spawned as well as picked again pawns;
-function onMouseMove(event) {
+function onMouseMove(dragged) {
     //this is important, it prevents text higlighting
     event.preventDefault;
 
