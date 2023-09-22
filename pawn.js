e091ef264dba74a1625531a2d1958f5cc01acad6
twofacefil11 <twofacefil11@gmail.com>
2023-09-22 22:21:54 +0000

    Invalid DateTime

--- pawn.js
+++ pawn.js
@@ -36,8 +36,8 @@ class Pawn {
             svg.style.left = this.xpos.toString() + 'px';
             svg.style.top = this.ypos.toString() + 'px';
 
-            svg.setAttributeMS(null, 'left', )
-            svg.setAttributeMS(null, 'left', )
+            svg.setAttributeMS(null, 'left', this.xpos.toString() + 'px');
+            svg.setAttributeMS(null, 'top', this.ypos.toString() + 'px');
 
         }
 
