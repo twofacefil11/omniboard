06c2d3e2e4878f74a1b3f07d51abf457f1364303
twofacefil11 <twofacefil11@gmail.com>
2023-09-23 02:16:45 +0000

    Invalid DateTime

--- pawn.js
+++ pawn.js
@@ -222,7 +222,7 @@ class Pawn {
         if (index !== -1)
             pawnsArr.splice(index, 1);
         else
-            console.log("what does not exist decided to unexist thyself once more smhw");
+            console.log("What does not exist decided to unexist thyself once more smhw");
     }
 
 
