1a16774846ee39d6251e61e0b57d9f07151a1516
twofacefil11 <twofacefil11@gmail.com>
2023-09-23 01:34:44 +0000

    Invalid DateTime

--- utils.js
+++ utils.js
@@ -28,6 +28,7 @@ function getAbsolutePosition(element) {
 
 //to know if you click within the radius of a pawn. to avoid picking by air
 function canPickup(p1, p2) {
+  console.log(er, Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2))
   return er >= Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
 }
 
