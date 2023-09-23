ad4f5c00da03973ef49dc4586e95473138868445
twofacefil11 <twofacefil11@gmail.com>
2023-09-23 19:00:52 +0000

    Invalid DateTime

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ blacksPicker.addEventListener("input", createBoard);
 columnsSldr.addEventListener("input", createBoard);
 rowsSldr.addEventListener("input", createBoard);
 
-document.addEventListener("resize", createBoard);
+document.addEventListener("resize", () => 
+    console.log(window.innerWidth, window.inneroHeight));
 
 //spacing action:
 boardRadiusSldr.addEventListener("input", createBoard);
