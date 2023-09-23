1d5ae9bcdd4981bdcbc9467bb1e29c9d08876b6f
twofacefil11 <twofacefil11@gmail.com>
2023-09-23 21:58:12 +0000

    Invalid DateTime

--- listeners.js
+++ listeners.js
@@ -7,8 +7,8 @@ function activateSpawner(spawner, color) {
         //get the position so you can create the pawn
 
         spawner.children[0].style.transform = 'scale(0.8)';
-        spawner.children[0].style.transition = 'transform 2s'
         spawner.children[0].style.transform = 'scale(1)';
+        spawner.children[0].style.transition = 'transform 2s'
         // spawner.style.transform = 'scale(1)'
         console.log(spawner)
 
