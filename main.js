f2ebc0008bbcdd8e39794b5a023f9bc6178244f0
twofacefil11 <twofacefil11@gmail.com>
2023-09-23 03:31:35 +0000

    Invalid DateTime

--- main.js
+++ main.js
@@ -70,6 +70,11 @@ function update(r) {
     
     blacksSpawn.currentCollor = whitesPicker.value;
     whitesSpawn.currentCollor = blacksPicker.value;
+
+
+    centers.forEach((c, i ,a) => {
+        
+    })
 }
 
 
