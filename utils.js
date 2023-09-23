36a87b4b9a3855e41fe8e1c8a8ce7bf865cbd3b4
twofacefil11 <twofacefil11@gmail.com>
2023-09-23 16:36:58 +0000

    Invalid DateTime

--- utils.js
+++ utils.js
@@ -96,24 +96,14 @@ function fillSquarePositions(c, r) {
   }  
 }
 
-function findTheClosestSquare() {
-  const pl = dragged.style.left;
-  const pt = dragged.style.top;
-  console.log(pl, pt)
+function findTheClosestSquare(event) {
+  const pl = event.clientX;
+  const pt = event.lientY;
   
   const closestIndex = squaresPositions.reduce((bestI, curr, i) => {
     const distance = Math.sqrt((pl - curr.x) ** 2 + (pt - curr.y) ** 2);
     const closestDistance = Math.sqrt((pl - squaresPositions[bestI].x) ** 2 + (pt - squaresPositions[bestI].y) ** 2);
-    console.log(distance, closestDistance);
-    
     return distance < closestDistance ? i : bestI;
   }, 0);
-
-  // const closestIndex = arrayOfObjects.reduce((closestIdx, currentObj, currentIndex) => {
-  //   const distance = Math.sqrt((givenX - currentObj.x) ** 2 + (givenY - currentObj.y) ** 2);
-  //   const closestDistance = Math.sqrt((givenX - arrayOfObjects[closestIdx].x) ** 2 + (givenY - arrayOfObjects[closestIdx].y) ** 2);
-  //   return distance < closestDistance ? currentIndex : closestIdx;
-  // }, 0);
-  console.log(closestIndex)
   return squaresPositions[closestIndex];
 }
