782ab1fe562eaa18bc19a2a6036766720fad9c6c
twofacefil11 <twofacefil11@gmail.com>
2023-10-10 15:20:58 +0000

    Invalid DateTime

--- main.js
+++ main.js
@@ -82,16 +82,16 @@ function createBoard() {
     board.style.backgroundColor = boardClrPicker.value;
 
     board.style.borderRadius = boardRadiusSldr.value + 'px';
-    // board.style.borderBottom = `2vh groove ${boardClrPicker.value}`;
-    // board.style.borderRight = `2vh groove ${boardClrPicker.value}`;
-    // board.style.borderLeft = `2vh ridge ${boardClrPicker.value}`;
-    // board.style.borderTop = `2vh ridge ${boardClrPicker.value}`;
+    board.style.borderBottom = `2vh groove ${boardClrPicker.value}`;
+    board.style.borderRight = `2vh groove ${boardClrPicker.value}`;
+    board.style.borderLeft = `2vh ridge ${boardClrPicker.value}`;
+    board.style.borderTop = `2vh ridge ${boardClrPicker.value}`;
 
     //not shure if i like it
-    dropzone.style.borderBottom = `15px ridge ${boardClrPicker.value}`;
-    dropzone.style.borderLeft = `15px groove ${boardClrPicker.value}`;
-    dropzone.style.borderRight = `15px ridge ${boardClrPicker.value}`;
-    dropzone.style.borderTop = `15px groove ${boardClrPicker.value}`;
+    // dropzone.style.borderBottom = `15px ridge ${boardClrPicker.value}`;
+    // dropzone.style.borderLeft = `15px groove ${boardClrPicker.value}`;
+    // dropzone.style.borderRight = `15px ridge ${boardClrPicker.value}`;
+    // dropzone.style.borderTop = `15px groove ${boardClrPicker.value}`;
 
     //clear all
     board.innerHTML = '';
