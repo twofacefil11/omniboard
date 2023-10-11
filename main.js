03b8dc0498f0c3d94d0b345e80af3df18befdd65
twofacefil11 <twofacefil11@gmail.com>
2023-10-11 16:45:46 +0000

    Invalid DateTime

--- main.js
+++ main.js
@@ -153,7 +153,7 @@ function createBoard() {
             else
                 square.style.backgroundColor = blacks;
             //board radius to corners //było 23 w px
-            let br = (parseInt(boardRadiusSldr.value)).toString();
+            let br = (parseInt(boardRadiusSldr.value) + 8).toString();
             if (i == 0) {
                 if (j == 0)
                     square.style.borderRadius = br + '% 0% 0% 0%';
